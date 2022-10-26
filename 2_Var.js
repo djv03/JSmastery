348b9f797d01a62fe6e4abdc0d98f38449961280
djv03 <93385841+djv03@users.noreply.github.com>
2022-10-26 16:10:18 +0530

    Add files via upload

---
+++ 2_Var.js
@@ -0,0 +1,26 @@
+
+//this is tutorial about usage of var
+
+"use strict";
+//above line is use for making our programme less errorful and make syntax correct
+
+//var is use for declaring variables,it is as same as x,y in maths
+
+var MyName = "Dhruvin";
+//here a memory block is alloted to variable with Myname
+console.log(MyName);
+
+
+//now as I have said this variable is same as maths so, x can change their variable according to condition
+MyName= "Vipul" ;
+//here MyName had value Dhruvin but now Vipul is overwrite on the MyName
+//so below line will print Vipul
+console.log(MyName);
+
+//***************************************************************************************************************************** */
+ //manipulate the variable while printing
+
+
+ var power = 5;
+ // '**' operator is use to  */
+ console.log(power**3);
