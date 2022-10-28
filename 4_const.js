a91af0c2d6cd0b3100720afd853995152ba4fb7a
djv03 <93385841+djv03@users.noreply.github.com>
2022-10-28 11:52:31 +0530

    Add files via upload

---
+++ 4_const.js
@@ -0,0 +1,12 @@
+//this is tutorrial about const keyword
+
+
+const pi=3.14;
+//once the variable is declared with const it canot be changed
+//for ex: const pi=3;
+//         will throw error 
+
+console.log(pi);
+
+//but you can manipulate the value by doing arithmatic opearation on it
+console.log((pi*2)+5);
