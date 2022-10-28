4c66f420568004ec21e5bf489b5a6f0a6657701c
djv03 <93385841+djv03@users.noreply.github.com>
2022-10-28 11:52:12 +0530

    Add files via upload

---
+++ 3_let.js
@@ -0,0 +1,16 @@
+//this tutorial is about let
+
+//let is introdunced in  ES6 version of JS 
+//Till then only var is use to declare variabel but it had some problems
+//the problems are:
+
+
+
+let MyName ="Dhruvin";
+console.log(MyName);
+
+// let MyName="Vipul";
+//the above line will thow error because let is used to declare varible only once 
+ MyName="Vipul";
+
+ console.log(MyName);
