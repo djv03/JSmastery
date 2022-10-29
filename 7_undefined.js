352b5d3e919f489dea9af89365cecf55730f3ef2
djv03 <93385841+djv03@users.noreply.github.com>
2022-10-29 10:01:06 +0530

    Add files via upload

---
+++ 7_undefined.js
@@ -0,0 +1,11 @@
+//undefined
+let string;
+console.log(typeof string);//---------->this will print undefined
+ 
+//null
+let MyName= null;
+console.log(typeof MyName);//----------->this will print object, which is sounds might consfusing
+                                         //this is bug in javascript and we cannot do anything with this
+
+//Bigint
+//use to store vry big value in integer formate
