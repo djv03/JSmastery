d06ad95fc617c000baf9fb4fc9bdb94367136e79
djv03 <93385841+djv03@users.noreply.github.com>
2022-10-31 22:20:51 +0530

    Add files via upload

---
+++ 8_comaparator.js
@@ -0,0 +1,20 @@
+//this is tutoruial about comparator oprator
+
+
+let num1= "7";
+let num2= 7;
+
+// 1. == vs ===
+
+console.log(num1==num2); //-------->print true
+ //because num1 and num2 are compared on the basis of storing values
+console.log(num1===num2); //-------->print true
+//beacuse num1 and num is compared on the basis of absolute values
+
+//!!!!!very imp: this nature of == and === is only observed in JS not in any other programming language
+
+//******************************************************************************** */
+//2. != vs !==
+
+console.log(num1!=num2);//check only by thing stored, therefore print false
+console.log(num1!==num2);//check by absolute value. therefore print true 
