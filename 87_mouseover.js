0f0baaf10c64dbf06a8985979fa4f4a823e7dd30
Dhruvin Vekariya <93385841+djv03@users.noreply.github.com>
2022-12-05 22:49:58 +0530

    Add files via upload

---
+++ 87_mouseover.js
@@ -0,0 +1,9 @@
+
+
+button.addEventListener("mouseover",function(){
+    console.log("cursor is over the button");   
+})
+
+button.addEventListener("mouseleave",function(){
+    console.log("cursor is outside the button");   
+})
